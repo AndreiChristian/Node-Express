79d5e5eedf2f5098ed005bf3e075dbde9aff20b3
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-21 16:36:31 +0200

    added update

--- REST_IMPROVED/controllers/feed.js
+++ REST_IMPROVED/controllers/feed.js
@@ -53,8 +53,6 @@ exports.createPosts = (req, res, next) => {
 };
 
 exports.getPost = (req, res, next) => {
-  // parse the postid from the url
-
   const postId = req.params.postId;
 
   if (!postId) {
@@ -82,9 +80,43 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
 
-  // error handling if there is no postid
-  // find the object with that id in the db
-  // error handling if there is no such object in the db
-  // return it as json
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, data is wrong");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+  const imageUrl = req.body.imageUrl;
+  const creator = req.body.creator;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post updated successfully" });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
