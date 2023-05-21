127e345f1fe909082df3684c2df3e98d8aa9e30e
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-21 20:03:54 +0200

    setup

---
+++ EVENTS/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "success",
+  });
+});
+
+app.listen(8080);
