14824416fa03987310cc90838376a1445cfa5f55
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-19 12:26:33 +0200

    basic setup

---
+++ JWT/backend/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json("Hello");
+});
+
+mongoose
+  .connect(
+    "mongodb://mongo:L2uB3h2FFSdP1h2TfqoR@containers-us-west-96.railway.app:5855"
+  )
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("Connected to the db and started app on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
